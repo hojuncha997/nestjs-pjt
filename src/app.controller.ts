f7efbab3fc8727fd0efa65a0f86e7fcec4b21f6f
hojuncha997 <hojun.cha997@gmail.com>
2024-05-18 19:53:45 +0900

    feat: 리다이렉션 API 기능 테스트
    app.controllers.ts에 리다이렉션 컨트롤러 생성하여 테스트 후 주석처리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import {Request} from 'express'
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,7 +7,18 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  // @Get('/hello')
   getHello(@Req() req: Request): string {
     return this.appService.getHello();
   }
+
+
+
+  // @Get("redirect/docs")
+  // @Redirect('https://docs.nestjs.com', 302)
+  // getDocs(@Query('version') version) {
+  //   return {url: 'https://docs.nestjs.com/v5/'}
+  // }
+  // // 리다이렉트 테스트: http://localhost:3000/redirect/docs?version=5 를 입력하면 https://docs.nestjs.com/v5로 리다이렉트
+
 }

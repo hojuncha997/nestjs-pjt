65ae900cd134ef39cb7919ca606dedd273f3164f
hojuncha997 <hojun.cha997@gmail.com>
2024-05-18 20:01:45 +0900

    feat: 라우팅 순서 수정
    app.controller.ts에 동일한 엔드포인트 있는 경우, api.controller.ts가 먼저 받도록 app.modules.ts 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,11 @@ import { ApiController } from './api/api.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController, ApiController],
+  controllers: [ApiController, AppController, UsersController ],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+// 동일한 라우팅 경로를 가진 엔드포인트가 존재할 경우
+// ApiController가 AppController보다 먼저 처리되도록 배열 순서 수정
